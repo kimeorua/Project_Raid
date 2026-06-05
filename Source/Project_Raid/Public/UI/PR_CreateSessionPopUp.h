495f1af8034022091d67c2bd8f8a7ee7045b7823
kimeorua <kimeorua1222@naver.com>
2026-06-05 10:04:11 +0900

    Class 생성 - PopUpUI

---
+++ Source/Project_Raid/Public/UI/PR_CreateSessionPopUp.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "PR_CreateSessionPopUp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_RAID_API UPR_CreateSessionPopUp : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+	
+};
