ab1f6e889318ac110d8731c95878ffbfdf58cb1d
kimeorua <kimeorua1222@naver.com>
2026-06-10 12:48:10 +0900

    Class 추가

---
+++ Source/Project_Raid/Public/PlayerController/APR_LobbyPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "APR_LobbyPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_RAID_API AAPR_LobbyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
