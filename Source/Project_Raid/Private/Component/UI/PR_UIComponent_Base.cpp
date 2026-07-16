aa383588ecb28db4473283c1193ad5c548758531
kimeorua <kimeorua1222@naver.com>
2026-07-16 12:42:36 +0900

    UIComponent_Base EndPlay 개선

--- Source/Project_Raid/Private/Component/UI/PR_UIComponent_Base.cpp
+++ Source/Project_Raid/Private/Component/UI/PR_UIComponent_Base.cpp
@@ -32,10 +32,27 @@ void UPR_UIComponent_Base::InitComponent()
 
 void UPR_UIComponent_Base::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
+	if (ASC)
+	{
+		const UPR_BasicAttributeSet* AttributeSet = Cast<UPR_BasicAttributeSet>(ASC->GetAttributeSet(UPR_BasicAttributeSet::StaticClass()));
+
+		if (AttributeSet)
+		{
+			if (HPChangedDelegateHandle.IsValid())
+			{
+				ASC->GetGameplayAttributeValueChangeDelegate(AttributeSet->GetHPAttribute()).Remove(HPChangedDelegateHandle);
+				HPChangedDelegateHandle.Reset();
+			}
+			
+			if (MaxHPChangedDelegateHandle.IsValid())
+			{
+				ASC->GetGameplayAttributeValueChangeDelegate(AttributeSet->GetMaxHPAttribute()).Remove(MaxHPChangedDelegateHandle);
+				MaxHPChangedDelegateHandle.Reset();
+			}
+		}
+	}
+
 	Super::EndPlay(EndPlayReason);
-	
-	HPChangedDelegateHandle.Reset();
-	MaxHPChangedDelegateHandle.Reset();
 }
 
 void UPR_UIComponent_Base::OnHPChanged(const FOnAttributeChangeData& Data)
